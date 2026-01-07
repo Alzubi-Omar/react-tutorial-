fae1811afc66f08c6c2487c4aacee0b7a499fd26
Alzubi-Omar <o.alzubi903@gmail.com>
2026-01-07 15:05:11 -0500

    start refactoring App to handle auth state

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,32 +1,42 @@
-import { ConditionalGreeting } from "./components/Elements/Elements";
 import { TodoList } from "./components/Lists/TodoList";
 import { Card } from "./components/Elements/Elements";
+import { useState } from "react";
+import { LoginForm } from "./components/auth/LoginForm";
 
 export default function App() {
-  const todos = [
-    {
-      id: 1,
-      text: "Learn React",
-      //   completed: false, (later use)
-    },
-    {
-      id: 2,
-      text: "Build a Todo App",
-    },
-    {
-      id: 3,
-      text: "Master JavaScript",
-    },
-  ];
+  // Authentication State Management
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  return (
-    <div>
-      <h1>Todo App</h1>
-      <ConditionalGreeting isLoggedIn name="Omar" />
+  // handle login
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
 
-      <Card title="My Todo">
-        <TodoList todos={todos} />
-      </Card>
-    </div>
-  );
+  // const todos = [
+  //   {
+  //     id: 1,
+  //     text: "Learn React",
+  //     //   completed: false, (later use)
+  //   },
+  //   {
+  //     id: 2,
+  //     text: "Build a Todo App",
+  //   },
+  //   {
+  //     id: 3,
+  //     text: "Master JavaScript",
+  //   },
+  // ];
+
+  if (!isAuthenticated) {
+    return <LoginForm onLogin={handleLogin} />;
+  }
+  return <div>Welcome to the Todo App!</div>;
+  // <div>
+  //   <h1>Todo App</h1>
+  //   <Card title="My Todo">
+  //     <TodoList todos={todos} />
+  //   </Card>
+  // </div>
+  // );
 }
