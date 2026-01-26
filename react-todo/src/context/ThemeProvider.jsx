14491d4998b60b38c17a0c69b65185acf9f528c0
Alzubi-Omar <o.alzubi903@gmail.com>
2026-01-26 12:57:31 -0500

    create Theme Provider and pass object via context

---
+++ react-todo/src/context/ThemeProvider.jsx
@@ -0,0 +1,16 @@
+import { useState, useMemo } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  const value = useMemo(() => ({ theme, toggleTheme }), [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
